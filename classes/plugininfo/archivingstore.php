a4152b9b41c70d9c9d1d8f2bffe0f4dc4349d065
Niels Gandraß <niels@gandrass.de>
2025-03-20 16:53:08 +0100

    Provide plugininfo classes for all subplugins

---
+++ classes/plugininfo/archivingstore.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Subplugin info class for archivingstore
+ *
+ * @package     local_archiving
+ * @category    plugininfo
+ * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_archiving\plugininfo;
+
+// @codingStandardsIgnoreLine
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+class archivingstore extends \core\plugininfo\base {
+
+    /**
+     * Should there be a way to uninstall the plugin via the administration UI.
+     *
+     * By default uninstallation is not allowed, plugin developers must enable it explicitly!
+     *
+     * @return bool
+     */
+    public function is_uninstall_allowed(): bool {
+        return true;
+    }
+
+    /**
+     * Whether this plugintype supports its plugins being disabled.
+     *
+     * @return bool
+     */
+    public static function plugintype_supports_disabling(): bool {
+        // TODO: Add support for enabling / disabling
+        return parent::plugintype_supports_disabling();
+    }
+
+}

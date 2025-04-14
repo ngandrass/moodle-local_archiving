c88b27b7a4c2767381646e705f263f12f4775cc4
Niels Gandraß <niels@gandrass.de>
2025-04-14 16:49:03 +0200

    Persist file handles in the database

--- classes/driver/store/file_handle.php
+++ classes/driver/store/file_handle.php
@@ -26,6 +26,9 @@
 namespace local_archiving\driver\store;
 
 // @codingStandardsIgnoreLine
+use local_archiving\type\db_table;
+use local_archiving\util\plugin_util;
+
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 
@@ -40,8 +43,17 @@ defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
  */
 final class file_handle {
 
+    /** @var int ID of this file handle */
+    protected int $id;
+
+    /** @var int ID of the archiving job this file is associated with */
+    protected int $jobid;
+
     /** @var string Name of the storage driver that works with this file handle */
-    protected string $archivingstore;
+    protected string $archivingstorename;
+
+    /** @var archivingstore|null Instance of the storage driver that is responsible for this file handle (lazy-loaded) */
+    protected ?archivingstore $archivingstore;
 
     /** @var string Name of the referenced file */
     protected string $filename;
@@ -58,22 +70,60 @@ final class file_handle {
     /**
      * Constructor
      *
-     * @param string $archivingstore Name of the storage driver that works with this file handle
+     * @param int $jobid ID of the archiving job this file is associated with
+     * @param string $archivingstorename Name of the storage driver that works with this file handle
      * @param string $filename Name of the referenced file
      * @param string $filepath Path of the referenced file
      * @param int $filesize Filesize in bytes
      * @param string $filekey Optional unique key for identifying the file
-     * @throws \coding_exception If given arguments are invalid
      */
-    public function __construct(
-        string $archivingstore,
+    protected function __construct(
+        int $id,
+        int $jobid,
+        string $archivingstorename,
         string $filename,
         string $filepath,
         int $filesize,
         string $filekey = ''
     ) {
+        $this->id = $id;
+        $this->jobid = $jobid;
+        $this->archivingstorename = $archivingstorename;
+        $this->filename = $filename;
+        $this->filepath = $filepath;
+        $this->filesize = $filesize;
+        $this->filekey = $filekey;
+    }
+
+    /**
+     * Creates a new file_handle and persists it inside the database
+     *
+     * @param int $jobid ID of the archiving job this file is associated with
+     * @param string $archivingstorename Name of the storage driver that works with this file handle
+     * @param string $filename Name of the referenced file
+     * @param string $filepath Path of the referenced file
+     * @param int $filesize Filesize in bytes
+     * @param string $filekey Optional unique key for identifying the file
+     * @return file_handle The created file handle
+     * @throws \coding_exception
+     * @throws \dml_exception
+     */
+    public static function create(
+        int $jobid,
+        string $archivingstorename,
+        string $filename,
+        string $filepath,
+        int $filesize,
+        string $filekey = ''
+    ): file_handle {
+        global $DB;
+
         // Validate arguments.
-        if (empty($archivingstore)) {
+        if ($jobid < 1) {
+            throw new \coding_exception(get_string('invalid_jobid', 'local_archiving'));
+        }
+
+        if (empty($archivingstorename) || !plugin_util::get_subplugin_by_name('archivingstore', $archivingstorename)) {
             throw new \coding_exception(get_string('invalid_archivingstore', 'local_archiving'));
         }
 
@@ -89,12 +139,109 @@ final class file_handle {
             throw new \coding_exception(get_string('invalid_filesize', 'local_archiving'));
         }
 
-        // Store arguments.
-        $this->archivingstore = $archivingstore;
-        $this->filename = $filename;
-        $this->filepath = $filepath;
-        $this->filesize = $filesize;
-        $this->filekey = $filekey;
+        // Insert into DB.
+        $id = $DB->insert_record(db_table::FILE_HANDLE, [
+            'jobid' => $jobid,
+            'archivingstore' => $archivingstorename,
+            'filename' => $filename,
+            'filepath' => $filepath,
+            'filesize' => $filesize,
+            'filekey' => $filekey,
+        ]);
+
+        return new self(
+            $id,
+            $jobid,
+            $archivingstorename,
+            $filename,
+            $filepath,
+            $filesize,
+            $filekey
+        );
+    }
+
+    /**
+     * Loads an existing file handle from the database
+     *
+     * @param int $id ID of the file handle to load
+     * @return file_handle The loaded file handle
+     * @throws \dml_exception
+     */
+    public static function get_by_id(int $id): file_handle {
+        global $DB;
+
+        $handle = $DB->get_record(db_table::FILE_HANDLE, ['id' => $id], '*', MUST_EXIST);
+        return new self(
+            $handle->id,
+            $handle->jobid,
+            $handle->archivingstore,
+            $handle->filename,
+            $handle->filepath,
+            $handle->filesize,
+            $handle->filekey
+        );
+    }
+
+    /**
+     * Retrieves all file handles associated with the given jobid
+     *
+     * @param int $jobid ID of the job to retrieve file handles for
+     * @return file_handle[] List of file handles associated with the job
+     * @throws \dml_exception
+     */
+    public static function get_by_jobid(int $jobid): array {
+        global $DB;
+
+        $handles = $DB->get_records(db_table::FILE_HANDLE, ['jobid' => $jobid]);
+
+        return array_map(fn ($handle) => new self(
+            $handle->id,
+            $handle->jobid,
+            $handle->archivingstore,
+            $handle->filename,
+            $handle->filepath,
+            $handle->filesize,
+            $handle->filekey
+        ), $handles);
+    }
+
+    /**
+     * Removes this file handle from the database and deletes the associated
+     * file, if desired
+     *
+     * @param bool $removefile If true, the referenced file will be deleted as well
+     * @return void
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    public function destroy(bool $removefile = false): void {
+        global $DB;
+
+        $DB->delete_records(db_table::FILE_HANDLE, ['id' => $this->id]);
+        if ($removefile) {
+            $this->archivingstore()->delete($this);
+        }
+    }
+
+    /**
+     * Returns an instance of the storage driver that is responsible for this
+     * file handle
+     *
+     * @return archivingstore Instance of the storage driver
+     * @throws \moodle_exception
+     */
+    public function archivingstore(): archivingstore {
+        if ($this->archivingstore instanceof archivingstore) {
+            return $this->archivingstore;
+        }
+
+        $driverclass = plugin_util::get_subplugin_by_name('archivingstore', $this->archivingstorename);
+        if (!$driverclass) {
+            throw new \moodle_exception('invalid_archivingstore', 'local_archiving');
+        }
+        $this->archivingstore = new $driverclass();
+
+        return $this->archivingstore;
     }
 
     /**

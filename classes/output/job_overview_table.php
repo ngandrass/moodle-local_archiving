f9885e49162655bf57a636f422b2c49bbb78e27b
Niels Gandraß <niels@gandrass.de>
2025-06-08 15:43:08 +0200

    Only allow download files for finished jobs

--- classes/output/job_overview_table.php
+++ classes/output/job_overview_table.php
@@ -182,8 +182,9 @@ class job_overview_table extends \table_sql {
         $html = '';
 
         // Action: Download.
+        $job = archive_job::get_by_id($values->id);
         $files = file_handle::get_by_jobid($values->id);
-        if (count($files) > 0) {
+        if ($job->is_completed() && count($files) > 0) {
             $downloadurl = new \moodle_url('/local/archiving/download.php', ['jobid' => $values->id]);
             // @codingStandardsIgnoreLine
             $html .= '<a href="'.$downloadurl.'" target="_blank" class="btn btn-success mx-1" role="button" data-toggle="tooltip" data-placement="top" title="'.get_string('download').'" alt="'.get_string('download').'"><i class="fa fa-download"></i></a>';

748fef1acda072585b683018a00902f43bc53acc
Niels Gandraß <niels@gandrass.de>
2025-10-25 13:41:34 +0200

    Fix namespace for admin_setting_webservice_enabler_test

--- tests/local/admin/setting/admin_setting_webservice_enabler_test.php
+++ tests/local/admin/setting/admin_setting_webservice_enabler_test.php
@@ -14,9 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-namespace local\admin\setting;
-
-use local_archiving\local\admin\setting\admin_setting_webservice_enabler;
+namespace local_archiving\local\admin\setting;
 
 /**
  * Tests for the admin_setting_webservice_enabler class.

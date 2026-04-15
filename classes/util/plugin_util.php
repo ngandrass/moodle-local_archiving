742560dcfb90e67faa7725e3d4025a37e14668fb
Björn Dittmann <bjoern.dittmann@haw-hamburg.de>
2026-04-15 11:03:43 +0200

    Stricter check to see if subplugin is installed on disk

--- classes/util/plugin_util.php
+++ classes/util/plugin_util.php
@@ -63,7 +63,7 @@ class plugin_util {
 
         // Iterate over all plugins and collect their metadata.
         foreach ($plugins as $plugin) {
-            if (!$plugin->rootdir) {
+            if (!isset($plugin->rootdir) || !file_exists($plugin->rootdir . '/version.php')) {
                 // Skip plugins with missing sources.
                 continue;
             }

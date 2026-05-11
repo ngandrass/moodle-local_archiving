06b4acafbc5670b26295c225910ab9ff9d7924a6
Björn Dittmann <bjoern.dittmann@haw-hamburg.de>
2026-05-11 18:54:03 +0200

    (port): Adapted interface of "process uploaded artifact" webservice to worker changes according to review

--- classes/external/process_uploaded_artifact.php
+++ classes/external/process_uploaded_artifact.php
@@ -100,14 +100,10 @@ class process_uploaded_artifact extends external_api {
                 'SHA256 checksum of the file',
                 VALUE_REQUIRED
             ),
-            // NOTE: We use an `external_value` type instead of a
-            // NOTE: `external_multiple_structure` type because of its request
-            // NOTE: overhead. Having a comma seperated value list reduces the
-            // NOTE: characters required for encoding the request in the URL.
-            'artifact_chunks' => new external_value(
-                PARAM_TEXT,
-                'filenames of individually uploaded chunks as comma seperated value list',
-                VALUE_OPTIONAL
+            'artifact_count' => new external_value(
+                PARAM_INT,
+                'Number of individually uploaded files to process',
+                VALUE_REQUIRED
             ),
         ]);
     }
@@ -140,7 +136,7 @@ class process_uploaded_artifact extends external_api {
      * @param string $artifactfilepathraw
      * @param int $artifactitemidraw
      * @param string $artifactsha256sumraw
-     * @param string $artifactchunksraw
+     * @param int $artifactcountraw
      * @return array
      * @throws \coding_exception
      * @throws \dml_exception
@@ -158,7 +154,7 @@ class process_uploaded_artifact extends external_api {
         string $artifactfilepathraw,
         int $artifactitemidraw,
         string $artifactsha256sumraw,
-        string $artifactchunksraw,
+        int $artifactcountraw,
     ): array {
         // Validate request.
         $params = self::validate_parameters(self::execute_parameters(), [
@@ -172,7 +168,7 @@ class process_uploaded_artifact extends external_api {
             'artifact_filepath' => $artifactfilepathraw,
             'artifact_itemid' => $artifactitemidraw,
             'artifact_sha256sum' => $artifactsha256sumraw,
-            'artifact_chunks' => $artifactchunksraw,
+            'artifact_count' => $artifactcountraw,
         ]);
 
         // Find the task.
@@ -195,15 +191,14 @@ class process_uploaded_artifact extends external_api {
 
         // Get or reconstruct uploaded file/-s.
         $draftfile = null;
-        if (!isset($artifactchunks) || $artifactchunks != "") {
+        if ($params['artifact_count'] > 1) {
             // Reassabmle orgininal file.
-            $chunkfilenames = explode(",", $artifactchunksraw);
             $draftfile = file_reassembler::reassamble_chunked_file(
                 $params['artifact_contextid'],
                 $params['artifact_itemid'],
                 $params['artifact_filepath'],
                 $params['artifact_filename'],
-                $chunkfilenames,
+                $params['artifact_count']
             );
             if (!$draftfile) {
                 $task->set_status(activity_archiving_task_status::FAILED);

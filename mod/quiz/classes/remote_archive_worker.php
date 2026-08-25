6326b6f4467f47e03258d5ae6d9f6b613ae462e1
Björn Dittmann <bjoern.dittmann@haw-hamburg.de>
2026-08-25 22:16:40 +0200

    (port) Feature: Add API option for including file hashes in archives.

--- mod/quiz/classes/remote_archive_worker.php
+++ mod/quiz/classes/remote_archive_worker.php
@@ -193,6 +193,7 @@ class remote_archive_worker {
                 "paper_format" => $settings->paper_format,
                 "archive_filename" => $task->get_job()->generate_archive_name_prefix(),
                 "archive_flatten" => (bool) $settings->archive_flat_export,
+                "archive_filehashes" => true,
                 "foldername_pattern" => $settings->attempt_foldername_pattern,
                 "filename_pattern" => $settings->attempt_filename_pattern,
                 "image_optimize" => $settings->image_optimize ? [

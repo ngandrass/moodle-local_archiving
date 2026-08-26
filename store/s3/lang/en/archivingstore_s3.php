124e4f69b0a8b959dce9655f5f3b7e54edbf081c
Niels Gandraß <niels@gandrass.de>
2026-08-26 17:31:30 +0200

    Create basic s3 config values

--- store/s3/lang/en/archivingstore_s3.php
+++ store/s3/lang/en/archivingstore_s3.php
@@ -25,9 +25,32 @@
 // @codingStandardsIgnoreFile
 
 // Common
-$string['pluginname'] = 'S3 Object Store';
+$string['pluginname'] = 'Object Store (S3)';
 $string['privacy:metadata'] = 'All data stored via this storage driver remains owned by local_archiving and will be handled directly by its privacy API instead.';
 
 // Settings.
 $string['setting_enabled'] = 'Enabled';
 $string['setting_enabled_desc'] = 'Enables or disables this storage driver. If disabled, no archives can be sent to or retrieved from this storage.';
+$string['setting_header_connection'] = 'Connection';
+$string['setting_header_connection_desc'] = 'Configure the connection to your S3-compatible object storage endpoint. This works with self-hosted S3-compatible services such as RustFS as well as AWS S3.';
+$string['setting_endpoint'] = 'Endpoint';
+$string['setting_endpoint_desc'] = 'Hostname of the S3 endpoint, optionally followed by a port. Do not include a scheme (http:// / https://) or bucket path here. Examples: <code>rustfs.example.com:9000</code> or <code>s3.eu-central-1.amazonaws.com</code>.';
+$string['setting_region'] = 'Region';
+$string['setting_region_desc'] = 'S3 region to use for request signing. Many self-hosted S3-compatible services accept an arbitrary value here, but a value is always required.';
+$string['setting_use_tls'] = 'Use TLS';
+$string['setting_use_tls_desc'] = 'If enabled, connections to the S3 endpoint are made via HTTPS instead of HTTP.';
+$string['setting_verify_tls'] = 'Verify TLS certificate';
+$string['setting_verify_tls_desc'] = 'If enabled, the TLS certificate presented by the S3 endpoint is verified. <strong>Disabling this reduces security</strong> and should only be done for self-hosted deployments with self-signed certificates that you fully trust.';
+$string['setting_path_style'] = 'Use path-style addressing';
+$string['setting_path_style_desc'] = 'If enabled, the bucket name is included in the request path (<code>https://endpoint/bucket/key</code>) instead of the hostname (<code>https://bucket.endpoint/key</code>). Self-hosted S3-compatible services such as MinIO or Ceph often require path-style addressing.';
+$string['setting_header_bucket'] = 'Bucket & Credentials';
+$string['setting_header_bucket_desc'] = 'Configure the bucket to store created archives in and the credentials used to access it.';
+$string['setting_bucket_path'] = 'Bucket path';
+$string['setting_bucket_path_desc'] = 'The bucket to store archives in, optionally followed by a key prefix (subfolder). Format: <code>bucket</code> or <code>bucket/optional/prefix</code>.<br>Example: <code>mynicebucket/folder/subfolder</code>.';
+$string['setting_access_key'] = 'Access key';
+$string['setting_access_key_desc'] = 'The access key used to authenticate with the S3 endpoint.';
+$string['setting_secret_key'] = 'Secret key';
+$string['setting_secret_key_desc'] = 'The secret key used to authenticate with the S3 endpoint.';
+$string['setting_connection_status'] = 'Connection status';
+$string['setting_connection_status_desc'] = 'Shows whether the settings below are complete and whether the configured endpoint and bucket are reachable. This check is performed live whenever this page is loaded, using the settings as of the last save.';
+
--- store/s3/settings.php
+++ store/s3/settings.php
@@ -22,6 +22,9 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use archivingstore_s3\local\admin\setting\admin_setting_s3_bucket_path;
+use archivingstore_s3\local\admin\setting\admin_setting_s3_endpoint;
+
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 
@@ -38,6 +41,78 @@ if ($hassiteconfig) {
             get_string('setting_enabled', 'archivingstore_s3'),
             get_string('setting_enabled_desc', 'archivingstore_s3'),
             '0'
+        ));
+
+        // Connection.
+        $settings->add(new admin_setting_heading(
+            'archivingstore_s3/header_connection',
+            get_string('setting_header_connection', 'archivingstore_s3'),
+            get_string('setting_header_connection_desc', 'archivingstore_s3')
+        ));
+
+        $settings->add(new admin_setting_s3_endpoint(
+            'archivingstore_s3/endpoint',
+            get_string('setting_endpoint', 'archivingstore_s3'),
+            get_string('setting_endpoint_desc', 'archivingstore_s3'),
+            ''
+        ));
+
+        $settings->add(new admin_setting_configtext(
+            'archivingstore_s3/region',
+            get_string('setting_region', 'archivingstore_s3'),
+            get_string('setting_region_desc', 'archivingstore_s3'),
+            'eu-central-1',
+            PARAM_RAW_TRIMMED
+        ));
+
+        $settings->add(new admin_setting_configcheckbox(
+            'archivingstore_s3/use_tls',
+            get_string('setting_use_tls', 'archivingstore_s3'),
+            get_string('setting_use_tls_desc', 'archivingstore_s3'),
+            '1'
+        ));
+
+        $settings->add(new admin_setting_configcheckbox(
+            'archivingstore_s3/verify_tls',
+            get_string('setting_verify_tls', 'archivingstore_s3'),
+            get_string('setting_verify_tls_desc', 'archivingstore_s3'),
+            '1'
+        ));
+
+        $settings->add(new admin_setting_configcheckbox(
+            'archivingstore_s3/path_style',
+            get_string('setting_path_style', 'archivingstore_s3'),
+            get_string('setting_path_style_desc', 'archivingstore_s3'),
+            '1'
+        ));
+
+        // Bucket & credentials.
+        $settings->add(new admin_setting_heading(
+            'archivingstore_s3/header_bucket',
+            get_string('setting_header_bucket', 'archivingstore_s3'),
+            get_string('setting_header_bucket_desc', 'archivingstore_s3')
+        ));
+
+        $settings->add(new admin_setting_s3_bucket_path(
+            'archivingstore_s3/bucket_path',
+            get_string('setting_bucket_path', 'archivingstore_s3'),
+            get_string('setting_bucket_path_desc', 'archivingstore_s3'),
+            ''
+        ));
+
+        $settings->add(new admin_setting_configtext(
+            'archivingstore_s3/access_key',
+            get_string('setting_access_key', 'archivingstore_s3'),
+            get_string('setting_access_key_desc', 'archivingstore_s3'),
+            '',
+            PARAM_RAW_TRIMMED
+        ));
+
+        $settings->add(new admin_setting_configpasswordunmask(
+            'archivingstore_s3/secret_key',
+            get_string('setting_secret_key', 'archivingstore_s3'),
+            get_string('setting_secret_key_desc', 'archivingstore_s3'),
+            ''
         ));
     }
 
